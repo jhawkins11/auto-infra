e3cdf6a5f89ad38f6a19c28cf6106c7b8bcb2dd0
jhawkins11 <josiah.c.hawkins@gmail.com>
2025-08-19 10:55:04 -0500

    refactor jest configuration to escape backslashes in moduleNameMapper patterns

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,19 +1,18 @@
-/* eslint-disable no-useless-escape */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 const config = {
   // use babel-jest to transform TypeScript
   testEnvironment: 'node',
   transform: {
-    '^.+\.(ts|tsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'babel-jest',
   },
   moduleNameMapper: {
-    '^(?:.*?/)?commands/(.*)\.js$': '<rootDir>/commands/$1.ts',
-    '^(?:.*?/)?services/(.*)\.js$': '<rootDir>/services/$1.ts',
-    '^(?:.*?/)?prompts/(.*)\.js$': '<rootDir>/prompts/$1.ts',
-    '^(?:.*?/)?types/(.*)\.js$': '<rootDir>/types/$1.ts',
-    '^<rootDir>/(.*)\.js$': '<rootDir>/$1.ts',
-    '^\.\/fileWriter\.js$': '<rootDir>/services/fileWriter.ts',
-    '^\.\.\/\.\.\/utils\/(.*)\.js$': '<rootDir>/tests/utils/$1.ts',
+    '^(?:.*?/)?commands/(.*)\\.js$': '<rootDir>/commands/$1.ts',
+    '^(?:.*?/)?services/(.*)\\.js$': '<rootDir>/services/$1.ts',
+    '^(?:.*?/)?prompts/(.*)\\.js$': '<rootDir>/prompts/$1.ts',
+    '^(?:.*?/)?types/(.*)\\.js$': '<rootDir>/types/$1.ts',
+    '^<rootDir>/(.*)\\.js$': '<rootDir>/$1.ts',
+    '^\\.\\/fileWriter\\.js$': '<rootDir>/services/fileWriter.ts',
+    '^\\.\\.\\/\\.\\.\\/utils\\/(.*)\\.js$': '<rootDir>/tests/utils/$1.ts',
   },
 };
 
